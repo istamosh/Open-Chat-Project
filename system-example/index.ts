2383592d126fdb07d6192098ef738551ea6863f3
istamosh <t0pramen19@gmail.com>
2020-10-10 00:49:59 +0700

    Integrating Socket.io
    on my way to second phase, going to sleep right now, let's call it a day.

--- system-example/index.ts
+++ system-example/index.ts
@@ -1,9 +1,15 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 
+var io = require('socket.io')(http);
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 }); //this is for display
+
+io.on('connection', (socket) => {
+    console.log('an user has connected.');
+}) //socket io notify server about connected client
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
