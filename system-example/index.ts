e213a0e8b9a927e2fa3e352667496a30bcc91544
istamosh <t0pramen19@gmail.com>
2020-10-09 22:19:00 +0700

    Basic Web Framework fix
    made index.html and basic understanding of Socket.io

--- system-example/index.ts
+++ system-example/index.ts
@@ -2,9 +2,9 @@ var app = require('express')();
 var http = require('http').createServer(app);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
-});
+    res.sendFile(__dirname + '/index.html');
+}); //this is for display
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
-});
+}); //for port settings and console
