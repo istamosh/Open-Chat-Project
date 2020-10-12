8203e10bc2967cd4d3063a80b100365342e4a80d
istamosh <t0pramen19@gmail.com>
2020-10-12 17:10:31 +0700

    Finished example chat
    finished example chat, now moving to real implementation on different folder, and adding branch.
    Curious about:
    -Nicknames
    -Certain user connect/disconnect
    -Whispers
    -Chat bubble of different users, merge chat bubble from the same user
    -(someone) is typing... feature
    -Online list
    -Tag system?

--- system-example/index.ts
+++ system-example/index.ts
@@ -9,7 +9,17 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('an user has connected.');
-}) //socket io notify server about connected client
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg); //copies the sent message to console
+
+        io.emit('chat message', msg); //emit the sent message into index.html message appender
+    }); //copies user message content to console
+
+    socket.on('disconnect', () => {
+        console.log('user dc.');
+    });
+}); //socket io notify server about connected and disconnected client
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
