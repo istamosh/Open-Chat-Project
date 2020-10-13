a10aee61b6b3b40e2ca1859f6148c9f64b5102cb
istamosh <t0pramen19@gmail.com>
2020-10-13 20:56:14 +0700

    done basic understanding

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
 const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8')
+
+const httpServer = require('http').createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Content-Length', Buffer.byteLength(content))
+    res.end(content)
+})
+
+const io = require('socket.io')(httpServer)
+
+io.on('connect', socket => {
+    console.log('connect')
+})
+
+httpServer.listen(3000, () => {
+    console.log('listening on localhost:3000')
+})
