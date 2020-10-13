687e8ae82b3d940fdd4a40bd3c22883a89010df6
istamosh <t0pramen19@gmail.com>
2020-10-13 23:53:21 +0700

    back to React implementation
    - Safely recreated all the project npm installation files and modified the package.json back into its original, but still existed in main folder.
    - Deleted index.html, index.ts, and tsconfig.json in Main folder.
    - Added create-react-app module inside main folder, uninstalled socket.io, but added inside client folder named 'socket.io-client'
    - Adding client and server directory.
    - Adding client's React basic boilerplate, will add App folder later.
    - Modified server's package.json to be able to work with nodemon
    - merging all npm installation with '-s' only, not '-d' (cause it's confusing for me right now).
    - merging this branch back to main branch, will make a fresh branch tomorrow.
    
    Let's call it a day.

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+//#region Basic React Boilerplate Code
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// every React's file will be put inside App folder
+import App from './App'
+
+// and inject/import all the created code inside index.html's root section (index.html is inside public folder)
+ReactDOM.render(<App />, document.querySelector('#root'));
+//#endregion
