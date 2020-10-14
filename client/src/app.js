0fc3961c7b4a536d57be0057e102f62613226969
istamosh <t0pramen19@gmail.com>
2020-10-14 18:26:06 +0700

    Front basic scripts

---
+++ client/src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Join from './components/Join';
+import Chat from './components/Chat';
+
+// greets user with Join components and pass the inputted datas from login screen into desired folder/script and Chat section
+const App = () => (
+    <Router>
+        <Route path="/" exact component={Join} />
+        <Route path="/chat" component={Chat} />
+    </Router>
+);
+
+export default App;
