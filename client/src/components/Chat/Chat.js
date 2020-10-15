d8e586c50027fc4b7d36b87e8a0ec5894134ba06
istamosh <t0pramen19@gmail.com>
2020-10-15 18:32:35 +0700

    Update Chat.js
    merging to main

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,31 @@
-import React from 'react';
+// importing hooks for useEffect = side effect performer
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import ioClient from 'socket.io-client';
 
-// HTML sample code to be delivered into App.js
-const Chat = () => {
+// make empty variable for ioClient
+let socket;
+
+// using location probe for parsing purpose
+// queryString.parse() is used for parsing the const. data value and inserted in Object variable inside browser Inspect element.
+// make socket as endpoint variable
+// re-render the queryString using ENDPOINT & location.search inside an array so it will only create one instance of socket connection
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search)
+
+        socket = ioClient(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        console.log(socket);
+    }, [ENDPOINT, location.search])
+
     return (
         <h1>Chat</h1>
     )
