53db3376be4a5c7551aefdb27051944ad09b50be
istamosh <t0pramen19@gmail.com>
2020-11-10 19:15:57 +0700

    Update Chat.js
    message does not cleared after sending, going to fix shortly.

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -10,8 +10,8 @@ import Input from './Input/Input';
 import Messages from './Messages/Messages';
 import UsersList from './UsersList/UsersList';
 
-// deployed heroku endpoint 
-const ENDPOINT = 'https://react-istacord.herokuapp.com/'; 
+// deployed heroku endpoint https://react-istacord.herokuapp.com/
+const ENDPOINT = 'localhost:5000'; 
 
 // make empty variable for ioClient
 let socket;
